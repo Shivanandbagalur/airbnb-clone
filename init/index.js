340124507ca4798acdeb5a7472a7502d2d230a8b
Shivananda <shivubagaloor12@gmail.com>
2025-05-15 17:39:46 +0530

    Initial commit of  Airbnb project

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6746ef1d1946e3855c019c48",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was intialized");
+};
+
+initDB();

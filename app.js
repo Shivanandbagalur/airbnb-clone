6c50eda243b07337db8dec6657e03ff67965199c
Shivananda <shivubagaloor12@gmail.com>
2025-08-17 23:09:37 +0530

    Added favicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
-
+app.use(express.static("public"));
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
